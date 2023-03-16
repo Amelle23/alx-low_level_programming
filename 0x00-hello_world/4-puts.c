c4ccacc4fcb00ce6a1c3dbd242f3eb18a3af3649
Amal Refaei <amalrefaei0@gmail.com>
2023-03-16 16:20:22 -0700

    betty styole in puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
 *main -  makes you enter the program
-*return -  return the value or outcome
+*Return:  return the value or outcome
 */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
