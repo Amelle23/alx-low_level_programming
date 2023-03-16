051fddb96d5e75c0bbba0aec8dcd9cb4294240e0
Amal Refaei <amalrefaei0@gmail.com>
2023-03-16 16:22:09 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - enters the program
- *return: returns a success value
+ *Return: returns a success value
 */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
