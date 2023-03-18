a55791dee679ab1430e2d5f198633f83e28c8b0b
Amal Refaei <amalrefaei0@gmail.com>
2023-03-17 19:08:33 -0700

    all except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet excep e and q
+ * Return: always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
