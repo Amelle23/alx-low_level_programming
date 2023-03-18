cd994b5a518cacc2a162f04b9c4d20191fd2f19f
Amal Refaei <amalrefaei0@gmail.com>
2023-03-17 19:04:21 -0700

    lower n upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Enters the program to print alphabet in lower then uppercase
+ * Return: Always zero
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
