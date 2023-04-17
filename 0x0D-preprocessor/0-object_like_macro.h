3757903227dec49a074af9b70cb0c728b576fffa
Amal Refaei <amalrefaei0@gmail.com>
2023-04-17 16:28:32 -0700

    object like header

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,14 +1,8 @@
 #ifndef _OBJECT_LIKE_
-#define _OBJECT_LIKE
+#define _OBJECT_LIKE_
 
 #define SIZE 1024
 
-/**
- * header file - defines a macro named SIZE as an abbreviation for the token 1024
- */
+/* This is a header file to define a macro named SIZE as an abbreviation for the token 1024 */
 
 #endif
-
-/**
- * end of conditional block
- */
