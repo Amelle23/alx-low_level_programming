1cc919b1b885166d061160cf8ad81bbfcaf6be0c
Amal Refaei <amalrefaei0@gmail.com>
2023-04-17 16:18:04 -0700

    Object like header

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef _OBJECT_LIKE_
+#define _OBJECT_LIKE
+
+#define SIZE 1024
+
+/**
+ * header file - defines a macro named SIZE as an abbreviation for the token 1024
+ */
+
+#endif
+
+/**
+ * end of conditional block
+ */
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is to resolve preprocessing tasks - Will get back later for a proper MD.
