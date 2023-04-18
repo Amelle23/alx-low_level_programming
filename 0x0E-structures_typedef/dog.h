c2b6a07b2ecb93e4fa1b258e282ae1a2d754b10e
Amal Refaei <amalrefaei0@gmail.com>
2023-04-17 18:47:22 -0700

    struct header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,14 +3,12 @@
 
 /**
  * dog_t - Typedef for struct dog
- * struct dog - it is used to store details about the dogs
+ * struct dog - it is used to sore details about the dogs
  * @name: name of pet
  * @age: age of pet
  * @owner: owner of pet
  * Description: struct willl be used here to inform us of the name, age and owner of the pet
  */
-
-typedef struct dog dog_t;
 
 struct dog
 {
@@ -19,6 +17,7 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
