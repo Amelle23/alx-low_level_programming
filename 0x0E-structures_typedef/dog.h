62d8fcc6efdc2e566391a1484654f9d74069743c
Amal Refaei <amalrefaei0@gmail.com>
2023-04-17 18:39:14 -0700

    updating dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - it is used to store details about the dogs
+ * @name: name of pet
+ * @age: age of pet
+ * @owner: owner of pet
+ * Description: struct willl be used here to inform us of the name, age and owner of the pet
+ */
 
 typedef struct dog dog_t;
 
@@ -14,13 +22,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-/**
- * struct dog - it is used to store details about the dogs
- * @name: name of pet
- * @age: age of pet
- * @owner: owner of pet
- * Description: struct willl be used here to inform us of the name, age and owner of the pet
- */
 
 #endif
