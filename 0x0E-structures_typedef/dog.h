588c30d82a0bc8bdaff6e6f41c110530e1417dfb
Amal Refaei <amalrefaei0@gmail.com>
2023-04-17 18:27:58 -0700

    poppy header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,12 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
+/**
+ * struct dog - it is used to store details about the dogs
+ * @name: name of pet
+ * @age: age of pet
+ * @owner: owner of pet
+ * Description: struct willl be used here to inform us of the name, age and owner of the pet
+ */
 
 #endif
