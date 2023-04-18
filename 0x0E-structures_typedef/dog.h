ba4b7ac7cfc9b2b5f98572527281229a89225e13
Amal Refaei <amalrefaei0@gmail.com>
2023-04-17 18:51:30 -0700

    modifying header adding space

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,18 @@
 #define _DOG_
 
 /**
- * dog_t - Typedef for struct dog
- * struct dog - it is used to sore details about the dogs
- * @name: name of pet
- * @age: age of pet
- * @owner: owner of pet
- * Description: struct willl be used here to inform us of the name, age and owner of the pet
+ * dog_t - Typedef for the struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
+ * struct dog - stores the dog's info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct that is called "dog" which stores the pet's details
  */
 
 struct dog
@@ -17,10 +23,10 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
+
